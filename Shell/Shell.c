6f7da1b095c055cc5a093f5051278d5f90a7de38
Buiscy <Aaron.regterschot@gmail.com>
2026-09-05 04:32:47 +1000

    Opening shell segment test

--- Shell/Shell.c
+++ Shell/Shell.c
@@ -1,7 +1,43 @@
 #include "main.h"
 
-int main (void)
+int main(int ac, char **av, char **env)
 {
-	printbanner();
+
+	(void)ac;
+	(void)env;
+
+	int j = 0; //Flag var for holding within shell
+	int i = 0; //Flag variable for skipping banner
+	char direct_buffer[1024];
+
+	while (*av != NULL)
+	{
+		if (strcmp(*av, "-ss") == 0)
+		{
+			i++;
+		}
+		av++;
+	}
+	printf("\n");
+	printf("\033[2J\033[H");
+
+	if (i == 0)
+	{
+		printbanner();
+		sleep(1);
+	}
+
+	if (getcwd(direct_buffer, sizeof(direct_buffer)) != NULL)
+	{
+		printf("%s$> ", direct_buffer);
+		fflush(stdout);
+	}
+	while (j == 0)
+	{
+		
+	}
+	
+
+	printf("\n");
 	return(0);
 }
